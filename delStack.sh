54e7e9d0ce991ac8e2d04a99d81a43a18367d495
hilyhoo@gmail.com <hilyhoo@gmail.com@fe644639-b9a8-df86-e7e9-15ee89ddf027>
2012-06-25 13:01:18 +0000

    Upload v0.1

---
+++ delStack.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -o xtrace
+
+## 
+apt-get remove -y keystone python-keystone python-keystoneclient
+apt-get remove -y glance glance-api glance-client glance-common glance-registry python-glance
+apt-get remove -y nova-api nova-cert nova-common nova-compute nova-compute-kvm nova-doc nova-network nova-objectstore nova-scheduler  nova-volume python-nova python-novaclient  nova-consoleauth python-novnc novnc
+apt-get remove -y libapache2-mod-wsgi openstack-dashboard
+
+## mysql
+MYSQL_PASSWD=${MYSQL_PASSWD:-"cloud1234"}
+mysql -uroot -p$MYSQL_PASSWD -e "DROP DATABASE IF EXISTS nova;"
+mysql -uroot -p$MYSQL_PASSWD -e "DROP DATABASE IF EXISTS glance;"
+mysql -uroot -p$MYSQL_PASSWD -e "DROP DATABASE IF EXISTS keystone;"
+#apt-get update
